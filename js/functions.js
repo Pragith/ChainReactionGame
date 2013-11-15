0c64f78ade6ce7a120da70e03632a583b90e9542
Pragith <pragith@gmail.com>
2013-11-15 15:30:19 +0530

    Working on recursion.

--- js/functions.js
+++ js/functions.js
@@ -9,39 +9,61 @@ function show_table(){
 		data += '</tr>';
 	}	
 	data += '</table>'
-	$("#refTable").append(data)
+	$("#gameTable").append(data)
 }
+
 
 
 function increase_cell_size(i,j){
 	//Increase till maximum cell size is reached		
 	if (store.get("cell"+i+j) < get_max_cell_size(i,j)){
 		store.set("cell"+i+j, store.get("cell"+i+j)+1)	 // Increase the size of current cell by 1
+		window.location.reload()
 	}
 	//Explode to other cells
 	else{
-		store.set("cell"+i+j, 0) //Set current cell to zero
+		//store.set("cell"+i+j, 0) //Set current cell to zero
 		explode(i,j)			 //Explode
 	}
 	// --- ISSUE - 1 Recursion required somewhere ---
 	//window.location.reload()
-	check_cells()
-}
-
-
-function check_cells(){
-	for (i=0;i<size;i++)
-		for (j=0;j<size;j++){
-			// Check each cell whether its current size is more than max size
-			if (store.get("cell"+i+j) > get_max_cell_size(i,j)){
-				explode(i,j)
-			}	
-	}		
-	window.location.reload()
 }
 
 
 
+function explode(i,j){
+
+	console.log(store.get("cell"+i+(j+1)))
+	console.log(get_max_cell_size(i,(j+1)))
+
+	if ((store.get("cell"+i+(j+1)) > get_max_cell_size(i,(j+1))) && (get_max_cell_size(i,j+1) != 'x')){
+		console.log("Inside")
+		store.set("cell"+i+(j+1), store.get("cell"+i+(j+1))+1)
+		store.set("cell"+i+j,0)
+		explode(i,(j+1))
+	}
+
+	if ((store.get("cell"+i+(j-1)) > get_max_cell_size(i,(j-1))) && (get_max_cell_size(i,j-1)  != 'x')){
+		console.log("Inside")
+		store.set("cell"+i+(j-1), store.get("cell"+i+(j-1))+1)
+		store.set("cell"+i+j,0)
+		explode(i,(j-1))
+	}
+	
+	if ((store.get("cell"+(i+1)+j) > get_max_cell_size((i+1),j)) && (get_max_cell_size(i+1,j) != 'x')){
+		console.log("Inside")
+		store.set("cell"+(i+1)+j, store.get("cell"+(i+1)+j)+1)
+		store.set("cell"+i+j,0)
+		explode((i+1),j)
+	}
+	
+	if ((store.get("cell"+(i-1)+j) > get_max_cell_size((i-1),j)) && (get_max_cell_size(i-1,j) != 'x')){
+		console.log("Inside")
+		store.set("cell"+(i-1)+j, store.get("cell"+(i-1)+j)+1)		
+		store.set("cell"+i+j,0)
+		explode((i-1),j)
+	}
+}
 
 
 
@@ -52,23 +74,9 @@ function check_cells(){
 
 // ---- MISC ----
 
-function explode(i,j){
-	if (get_max_cell_size(i,j+1) != 'x'){
-		store.set("cell"+i+(j+1), store.get("cell"+i+(j+1))+1)
-	}
-	if (get_max_cell_size(i,j-1)  != 'x'){
-		store.set("cell"+i+(j-1), store.get("cell"+i+(j-1))+1)
-	}
-	if (get_max_cell_size(i+1,j) != 'x'){
-		store.set("cell"+(i+1)+j, store.get("cell"+(i+1)+j)+1)
-	}
-	if (get_max_cell_size(i-1,j) != 'x'){
-		store.set("cell"+(i-1)+j, store.get("cell"+(i-1)+j)+1)
-	}	
-}
 
 function get_max_cell_size(i,j){
-	if (i == size || i == -1 || j == size || j == -1)
+	if (i == j == -1 || i == j == size)
 		return 'x'
 	if (i==0 || i==size-1 || j==0 || j==size-1)
 		return 2	
